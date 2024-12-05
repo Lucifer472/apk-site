0c9664215832dd7e864802557c3cc51c42c9cfe7
Lucifer472 <hardiksadhu472@gmail.com>
2024-12-05 12:07:04 +0530

    added all category & fix build errors

--- src/constant.ts
+++ src/constant.ts
@@ -50,8 +50,128 @@ export const appRatings = [
 
 export const allCategory = [
   {
-    label: "Art Design",
+    label: "Art & Design",
     value: "art-design",
+  },
+  {
+    label: "Auto & Vehicles",
+    value: "auto-vehicles",
+  },
+  {
+    label: "Beauty",
+    value: "beauty",
+  },
+  {
+    label: "Books & Reference",
+    value: "books-reference",
+  },
+  {
+    label: "Business",
+    value: "business",
+  },
+  {
+    label: "Comics",
+    value: "comics",
+  },
+  {
+    label: "Communication",
+    value: "communication",
+  },
+  {
+    label: "Dating",
+    value: "dating",
+  },
+  {
+    label: "Education",
+    value: "education",
+  },
+  {
+    label: "Entertainment",
+    value: "entertainment",
+  },
+  {
+    label: "Events",
+    value: "events",
+  },
+  {
+    label: "Health & Fitness",
+    value: "health-fitness",
+  },
+  {
+    label: "House & Home",
+    value: "house-home",
+  },
+  {
+    label: "Libraries & Demo",
+    value: "libraries-demo",
+  },
+  {
+    label: "Lifestyle",
+    value: "lifestyle",
+  },
+  {
+    label: "Medical",
+    value: "medical",
+  },
+  {
+    label: "Music & Audio",
+    value: "music-audio",
+  },
+  {
+    label: "News & Magazines",
+    value: "news-magazines",
+  },
+  {
+    label: "Parenting",
+    value: "parenting",
+  },
+  {
+    label: "Photography",
+    value: "photography",
+  },
+  {
+    label: "Productivity",
+    value: "productivity",
+  },
+  {
+    label: "Social",
+    value: "social",
+  },
+  {
+    label: "Sport",
+    value: "sport",
+  },
+  {
+    label: "Travel & Local",
+    value: "travel-local",
+  },
+  {
+    label: "Video Players & Editors",
+    value: "video-players-editors",
+  },
+  {
+    label: "Weather",
+    value: "weather",
+  },
+  {
+    label: "Tools",
+    value: "tools",
+  },
+  {
+    label: "Finance",
+    value: "finance",
+  },
+  {
+    label: "Food & Drink",
+    value: "food-drink",
+  },
+  {
+    label: "Maps & Navigation",
+    value: "maps-navigation",
+  },
+  {
+    label: "Shopping",
+    value: "shopping",
   },
   {
     label: "Battle Royal",
@@ -61,12 +181,200 @@ export const allCategory = [
     label: "Racing",
     value: "racing",
   },
+  {
+    label: "Action",
+    value: "action",
+  },
+  {
+    label: "Adventure",
+    value: "adventure",
+  },
+  {
+    label: "Arcade",
+    value: "arcade",
+  },
+  {
+    label: "Board",
+    value: "board",
+  },
+  {
+    label: "Card",
+    value: "card",
+  },
+  {
+    label: "Casino",
+    value: "casino",
+  },
+  {
+    label: "Casual",
+    value: "casual",
+  },
+  {
+    label: "Educational",
+    value: "educational",
+  },
+  {
+    label: "Music",
+    value: "music",
+  },
+  {
+    label: "Role Playing",
+    value: "role-playing",
+  },
+  {
+    label: "Simulation",
+    value: "simulation",
+  },
+  {
+    label: "Sports",
+    value: "sports",
+  },
+  {
+    label: "Strategy",
+    value: "strategy",
+  },
+  {
+    label: "Trivia",
+    value: "trivia",
+  },
+  {
+    label: "Word",
+    value: "word",
+  },
+  {
+    label: "Family",
+    value: "family",
+  },
+  {
+    label: "Puzzle",
+    value: "puzzle",
+  },
 ];
 
 export const appCategory = [
   {
-    label: "Art Design",
+    label: "Art & Design",
     value: "art-design",
+  },
+  {
+    label: "Auto & Vehicles",
+    value: "auto-vehicles",
+  },
+  {
+    label: "Beauty",
+    value: "beauty",
+  },
+  {
+    label: "Books & Reference",
+    value: "books-reference",
+  },
+  {
+    label: "Business",
+    value: "business",
+  },
+  {
+    label: "Comics",
+    value: "comics",
+  },
+  {
+    label: "Communication",
+    value: "communication",
+  },
+  {
+    label: "Dating",
+    value: "dating",
+  },
+  {
+    label: "Education",
+    value: "education",
+  },
+  {
+    label: "Entertainment",
+    value: "entertainment",
+  },
+  {
+    label: "Events",
+    value: "events",
+  },
+  {
+    label: "Health & Fitness",
+    value: "health-fitness",
+  },
+  {
+    label: "House & Home",
+    value: "house-home",
+  },
+  {
+    label: "Libraries & Demo",
+    value: "libraries-demo",
+  },
+  {
+    label: "Lifestyle",
+    value: "lifestyle",
+  },
+  {
+    label: "Medical",
+    value: "medical",
+  },
+  {
+    label: "Music & Audio",
+    value: "music-audio",
+  },
+  {
+    label: "News & Magazines",
+    value: "news-magazines",
+  },
+  {
+    label: "Parenting",
+    value: "parenting",
+  },
+  {
+    label: "Photography",
+    value: "photography",
+  },
+  {
+    label: "Productivity",
+    value: "productivity",
+  },
+  {
+    label: "Social",
+    value: "social",
+  },
+  {
+    label: "Sport",
+    value: "sport",
+  },
+  {
+    label: "Travel & Local",
+    value: "travel-local",
+  },
+  {
+    label: "Video Players & Editors",
+    value: "video-players-editors",
+  },
+  {
+    label: "Weather",
+    value: "weather",
+  },
+  {
+    label: "Tools",
+    value: "tools",
+  },
+  {
+    label: "Finance",
+    value: "finance",
+  },
+  {
+    label: "Food & Drink",
+    value: "food-drink",
+  },
+  {
+    label: "Maps & Navigation",
+    value: "maps-navigation",
+  },
+  {
+    label: "Shopping",
+    value: "shopping",
   },
 ];
 
@@ -78,6 +386,74 @@ export const gameCategory = [
   {
     label: "Racing",
     value: "racing",
+  },
+  {
+    label: "Action",
+    value: "action",
+  },
+  {
+    label: "Adventure",
+    value: "adventure",
+  },
+  {
+    label: "Arcade",
+    value: "arcade",
+  },
+  {
+    label: "Board",
+    value: "board",
+  },
+  {
+    label: "Card",
+    value: "card",
+  },
+  {
+    label: "Casino",
+    value: "casino",
+  },
+  {
+    label: "Casual",
+    value: "casual",
+  },
+  {
+    label: "Educational",
+    value: "educational",
+  },
+  {
+    label: "Music",
+    value: "music",
+  },
+  {
+    label: "Role Playing",
+    value: "role-playing",
+  },
+  {
+    label: "Simulation",
+    value: "simulation",
+  },
+  {
+    label: "Sports",
+    value: "sports",
+  },
+  {
+    label: "Strategy",
+    value: "strategy",
+  },
+  {
+    label: "Trivia",
+    value: "trivia",
+  },
+  {
+    label: "Word",
+    value: "word",
+  },
+  {
+    label: "Family",
+    value: "family",
+  },
+  {
+    label: "Puzzle",
+    value: "puzzle",
   },
 ];
 
